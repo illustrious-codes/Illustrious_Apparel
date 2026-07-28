11b0f814ed0b68ee315d88c0ef8ad3fc7144ec00
illustrious-codes <onatayodavid105@gmail.com>
2026-07-28 22:20:52 +0100

    dav

--- Naore/Naore/settings.py
+++ Naore/Naore/settings.py
@@ -45,9 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
 
     #Custom Apps
     'core', 
@@ -100,7 +97,6 @@ DATABASES = {
 }
 
 database_url = os.environ.get("DATABASE_URL")
-print(repr(database_url))
 DATABASES["default"] = dj_database_url.parse(database_url)
 
 
@@ -138,13 +134,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = 'static/'
 
